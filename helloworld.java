e90d8b1e9a03fe19a633ced5dd8aa243d5705751
Shibani-code1 <2021wa86116@wilp.bits-pilani.ac.in>
2025-07-10 01:56:51 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -2,5 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, world_01!");
         System.out.println("Hello, world_02!");
+        System.out.println("Hello, world_03!");
+        System.out.println("Hello, world_04!");
     }
 }
